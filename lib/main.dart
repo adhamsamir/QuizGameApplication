56b519b8ea2072d336d4d3c60483d59b388a0974
OmarSala7 <omar1803098@miuegypt.edu.eg>
2021-11-19 15:04:08 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+    (
+      title: 'Home',
+      home: Scaffold
+      (
+        drawer: Drawer
+        (
+          child: ListView
+          (
+            padding: EdgeInsets.zero,
+            children:
+            [
+              DrawerHeader
+              (
+                padding: EdgeInsets.zero,
+                decoration: BoxDecoration(color: Colors.teal),
+                child: Column
+                (
+                  children: 
+                  [
+                    Text
+                    (
+                      'Alan Turing',
+                      textAlign: TextAlign.start,
+                      style: TextStyle
+                      (
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.white
+                      )
+                    ),
+
+                    Text
+                    (
+                      'Level 41',
+                      textAlign: TextAlign.start,
+                      style: TextStyle
+                      (
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white
+                      )
+                    ),
+
+                    SizedBox(height: 40),
+
+                    Text
+                    (
+                      '16 correct answers to lvl.(42)',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle
+                      (
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white
+                      )
+                    )
+                  ],
+                )
+                
+                
+              ),
+
+              ListTile
+              (
+                title: const Text('Notifications'),
+                onTap: () {},
+              ),
+
+              ListTile
+              (
+                title: const Text('Sound'),
+                onTap: () {},
+              ),
+
+              ListTile
+              (
+                title: const Text('Vibration'),
+                onTap: () {},
+              ),
+
+              ListTile
+              (
+                title: const Text('Credits'),
+                onTap: () {},
+              ),
+
+              ListTile
+              (
+                title: const Text('Reset Game Progress'),
+                onTap: () {},
+              )
+            ],
+          )
+        ),
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.white,
+        appBar: AppBar
+        (
+          iconTheme: IconThemeData(color: Colors.teal),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center
+        (
+          child: Column
+          (
+            children: 
+            [
+              Image.asset('images/logo.png', scale: 1),
+              
+              const Text
+              (
+                'Computer Science Quiz',
+                textAlign: TextAlign.center,
+                style: TextStyle
+                (
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  ),
+                ),
+
+              SizedBox(height: 20),
+
+              ElevatedButton
+              (
+                onPressed: (){},
+                child: const Text('PLAY'),
+                style: ElevatedButton.styleFrom
+                (
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  primary: Colors.teal,
+                  textStyle: TextStyle(fontSize: 30)
+                ),
+              )
+            ],
+          )
+          
+          
+        ),
+      ),
+    );
+  }
+}
