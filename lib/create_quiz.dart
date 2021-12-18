6f33ea6b209427669872df125faf5cd92ff72517
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2021-12-18 16:34:14 +0200

    Update create_quiz.dart

--- lib/create_quiz.dart
+++ lib/create_quiz.dart
@@ -13,6 +13,60 @@ class CreateQuiz extends StatefulWidget {
 class _CreateQuizState extends State<CreateQuiz> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QuizMaker'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Form(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (val) =>
+                    val!.isEmpty ? "Enter Quiz Image Url" : null,
+                decoration:
+                    InputDecoration(hintText: "Quiz Image Url (Optional)"),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                validator: (val) => val!.isEmpty ? "Enter Quiz Title" : null,
+                decoration: InputDecoration(hintText: "Quiz Title"),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val!.isEmpty ? "Enter Quiz Description" : null,
+                decoration: InputDecoration(hintText: "Quiz Description"),
+              ),
+              Spacer(),
+              GestureDetector(
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text(
+                    "Create Quiz",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
