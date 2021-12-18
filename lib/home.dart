9ad5c4fa475accb52befa70d4b66093053469aac
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2021-12-18 15:29:25 +0200

    Update home.dart

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QuizMaker'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+    );
+  }
+}
