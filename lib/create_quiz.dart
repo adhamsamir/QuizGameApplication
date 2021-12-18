323aa853c7ffe3d4e1703446dfb6c49c097ca047
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2021-12-18 15:58:05 +0200

    Created add quiz screen

---
+++ lib/create_quiz.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CreateQuiz extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _CreateQuizState extends State<CreateQuiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home/create_quiz.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -16,6 +17,18 @@ class _HomeState extends State<Home> {
         title: const Text('QuizMaker'),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
+      ),
+      body: Container(
+        child: (Column(
+          children: [],
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CreateQuiz()));
+        },
       ),
     );
   }
